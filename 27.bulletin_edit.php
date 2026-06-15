a896e35d4124c473f495eabc6bfcd80d9637deb3
An0ther030 <whtc2222@gmail.com>
2026-06-15 14:37:50 +0800

    Update 27.bulletin_edit.php

--- 27.bulletin_edit.php
+++ 27.bulletin_edit.php
@@ -1,17 +1,34 @@
 <?php
 
+    // 關閉錯誤訊息並啟動 Session
     error_reporting(0);
     session_start();
+
+    // 檢查是否已登入
     if (!$_SESSION["id"]) {
         echo "請登入帳號";
         echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
     }
-    else{   
+    else{
+
+        // 連接資料庫
         $conn=mysqli_connect("120.105.96.90", "immust", "immustimmust", "immust");
-        if (!mysqli_query($conn, "update bulletin set title='{$_POST['title']}',content='{$_POST['content']}',time='{$_POST['time']}',type={$_POST['type']} where bid='{$_POST['bid']}'")){
+
+        // 更新指定公告資料
+        if (!mysqli_query($conn,
+            "update bulletin
+             set title='{$_POST['title']}',
+                 content='{$_POST['content']}',
+                 time='{$_POST['time']}',
+                 type={$_POST['type']}
+             where bid='{$_POST['bid']}'")){
+
+            // 修改失敗
             echo "修改錯誤";
             echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
         }else{
+
+            // 修改成功
             echo "修改成功，三秒鐘後回到佈告欄列表";
             echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
         }
